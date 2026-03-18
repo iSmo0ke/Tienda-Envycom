7e3f089cadc4ddc55f5b43e97814c8c557450956
Jesús Altamirano Carrillo <jesusaltamirano@envycom.mx>
2026-03-18 11:16:00 -0600

    Bug de imagen en carrito

--- resources/views/components/product-image.blade.php
+++ resources/views/components/product-image.blade.php
@@ -1,11 +1,18 @@
-@props(['image', 'alt', 'cssClass' => ''])
+@props(['image', 'alt' => '', 'cssClass' => ''])
 
-@if($image)
-    @if(\Illuminate\Support\Str::startsWith($image, ['http://', 'https://']))
-        <img src="{{ $image }}" alt="{{ $alt }}" class="{{ $cssClass }}">
-    @else
-        <img src="{{ asset('storage/' . $image) }}" alt="{{ $alt }}" class="{{ $cssClass }}">
-    @endif
-@else
-    <img src="{{ asset('img/logo.png') }}" alt="Sin imagen" class="{{ $cssClass }} object-contain bg-gray-100 p-4">
-@endif
+@php
+    // Lógica para determinar el origen de la imagen
+    $src = $image;
+
+    // Si no es una URL (no empieza con http), buscamos en el storage local
+    if ($image && !str_starts_with($image, 'http')) {
+        $src = asset('storage/' . $image);
+    }
+
+    // Imagen por defecto si no existe
+    if (!$image) {
+        $src = asset('img/icono-gris.jpg');
+    }
+@endphp
+
+<img src="{{ $src }}" alt="{{ $alt }}" class="{{ $cssClass }}" loading="lazy">
