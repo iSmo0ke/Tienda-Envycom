fde14764e8492acee4b2f8058aa1bc47199e48a0
Jesús Altamirano Carrillo <jesusaltamirano@envycom.mx>
2026-04-01 12:51:09 -0600

    Cambios de Oswa Sepomex

--- database/migrations/2026_03_12_154531_create_addresses_table.php
+++ database/migrations/2026_03_12_154531_create_addresses_table.php
@@ -8,19 +8,34 @@ return new class extends Migration
 {
     public function up(): void
     {
+        Schema::create('postal_codes', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('zip_code'); // d_codigo
+            $table->string('settlement'); // d_asenta
+            $table->string('settlement_type'); // d_tipo_asenta
+            $table->string('municipality'); // D_mnpio
+            $table->string('state'); // d_estado
+            $table->string('city')->nullable(); // d_ciudad
+            $table->timestamps();
+
+            // Índices para optimizar búsquedas
+            $table->index('zip_code');
+            $table->index('state');
+            $table->index('municipality');
+        });
+
         Schema::create('addresses', function (Blueprint $table) {
-            $table->id();
+            $table->id()->primary();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
-            $table->string('alias')->nullable(); // Ej: Casa, Oficina
-            $table->string('receptor_name'); // Quién recibe
-            $table->string('phone');
-            $table->string('calle_numero');
-            $table->string('colonia');
-            $table->string('municipio_alcaldia');
-            $table->string('estado');
-            $table->string('codigo_postal', 10);
-            $table->text('referencias')->nullable();
+            $table->foreignUuid('sepomex_id')->constrained('postal_codes');
+            $table->string('calle');
+            $table->string('alias')->nullable();
             $table->boolean('is_default')->default(false);
+            $table->string('numero_exterior')->nullable();
+            $table->string('receptor_name');
+            $table->string('numero_interior')->nullable();
+            $table->text('referencias')->nullable();
+            $table->string('telefono',12);
             $table->timestamps();
         });
     }
@@ -28,6 +43,7 @@ return new class extends Migration
     public function down(): void
     {
         Schema::dropIfExists('addresses');
+        Schema::dropIfExists('postal_codes');
     }
 };
 
